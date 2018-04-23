886b313c6976be85a8a358164e4d6cb914490b7a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Grid - How to change a column's filter popup using CustomColumnFilterPopupTemplate
+
+
+<p>The <a href="https://documentation.devexpress.com/WPF/DevExpress.Xpf.Grid.ColumnBase.CustomColumnFilterPopupTemplate.property">ColumnBase.CustomColumnFilterPopupTemplate</a> property allows you to use your own template for a column's filter popup. In this example, a custom popup is implemented that provides the capability to filter DateTime columns using a range (From - To). This popup contains two DateEdits.<br>The custom popup is a ContentControl's descendant with the Filter property. This property is bound to a column's filter.</p>
+
+<br/>
+
+
---
+++ VB/DateRangeFilterCS/Window1.xaml.vb
@@ -0,0 +1,91 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Wpf.Editors
+
+Namespace DateRangeFilterCS
+
+    Partial Public Class Window1
+        Inherits Window
+        Public Sub New()
+            InitializeComponent()
+
+            Dim list As New List(Of TestData)()
+            For month As Integer = 1 To 12
+                For day As Integer = 1 To 28
+                    list.Add(New TestData() With _
+                             {.TestDate = New DateTime(DateTime.Today.Year, month, day)})
+                Next day
+            Next month
+            gridControl.DataSource = list
+        End Sub
+    End Class
+
+    Public Class TestData
+        Private privateDate As DateTime
+        Public Property TestDate() As DateTime
+            Get
+                Return privateDate
+            End Get
+            Set(ByVal value As DateTime)
+                privateDate = value
+            End Set
+        End Property
+    End Class
+
+    Public Class DateEditFilter
+        Inherits ContentControl
+        Public Shared ReadOnly FilterProperty As DependencyProperty
+        Shared Sub New()
+            FilterProperty = DependencyProperty.Register("Filter", GetType(CriteriaOperator), _
+                              GetType(DateEditFilter), New FrameworkPropertyMetadata(Nothing))
+        End Sub
+
+        Public Property Filter() As CriteriaOperator
+            Get
+                Return CType(GetValue(FilterProperty), CriteriaOperator)
+            End Get
+            Set(ByVal value As CriteriaOperator)
+                SetValue(FilterProperty, value)
+            End Set
+        End Property
+
+        Private dateEditFrom As DateEdit
+        Private dateEditTo As DateEdit
+        Public Overrides Sub OnApplyTemplate()
+            MyBase.OnApplyTemplate()
+
+            If dateEditFrom IsNot Nothing Then
+                RemoveHandler dateEditFrom.EditValueChanged, AddressOf dateEdit_EditValueChanged
+            End If
+            If dateEditTo IsNot Nothing Then
+                RemoveHandler dateEditTo.EditValueChanged, AddressOf dateEdit_EditValueChanged
+            End If
+
+            dateEditFrom = TryCast(FindName("PART_DateEditFrom"), DateEdit)
+            dateEditTo = TryCast(FindName("PART_DateEditTo"), DateEdit)
+
+            Dim op As GroupOperator = TryCast(Filter, GroupOperator)
+            If op IsNot Nothing Then
+                dateEditFrom.EditValue = _
+                (TryCast((TryCast(op.Operands(0), BinaryOperator)).RightOperand, OperandValue)).Value
+                dateEditTo.EditValue = _
+                (TryCast((TryCast(op.Operands(1), BinaryOperator)).RightOperand, OperandValue)).Value
+            End If
+
+            AddHandler dateEditTo.EditValueChanged, AddressOf dateEdit_EditValueChanged
+            AddHandler dateEditFrom.EditValueChanged, AddressOf dateEdit_EditValueChanged
+        End Sub
+
+        Private Sub dateEdit_EditValueChanged(ByVal sender As Object, ByVal e As EditValueChangedEventArgs)
+            Filter = CriteriaOperator.And(New BinaryOperator("Date", dateEditFrom.EditValue, _
+                     BinaryOperatorType.GreaterOrEqual), New BinaryOperator("Date", dateEditTo.EditValue, _
+                     BinaryOperatorType.LessOrEqual))
+        End Sub
+
+    End Class
+
+End Namespace
